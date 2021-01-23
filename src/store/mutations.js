9dc087a6128d6cc432b53e0a69d1fd284ff6a137
Talha Karatas <ayasin0618@outlook.com>
2021-01-23 17:01:58 +0300

    local mutations adTask function fixed

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -10,14 +10,32 @@ export default {
   adTask(state, payload) {
     if (payload.completed) {
       const index = state.tasks.findIndex((task) => task._id == payload._id)
-      state.tasks.splice(index, 1)
+      if (index > -1) {
+        state.tasks.splice(index, 1)
+      }
       state.completedTasks.unshift(payload)
     } else {
       const index = state.completedTasks.findIndex(
         (task) => task._id == payload._id
       )
-      state.completedTasks.splice(index, 1)
-      state.tasks.unshift(payload)
+      if (index > -1) {
+        state.completedTasks.splice(index, 1)
+      }
+      if (state.tasks.length == 0) {
+        state.tasks.push(payload)
+      } else {
+        let count = 0
+        state.tasks.forEach((task, index) => {
+          if (Date.parse(payload.date) > Date.parse(task.date) && count == 0) {
+            count++
+            state.tasks.splice(index, 0, payload)
+          } else {
+            if (index + 1 === state.tasks.length) {
+              state.tasks.push(payload)
+            }
+          }
+        })
+      }
     }
   },
 
